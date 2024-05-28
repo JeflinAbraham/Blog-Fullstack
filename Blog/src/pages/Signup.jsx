9ccb1131af4bb43a6e6f8668341b065fa50138ff
JeflinAbraham <jeflinabraham21@gmail.com>
2024-05-28 23:28:19 +0530

    completed the signin page and functionality

--- Blog/src/pages/Signup.jsx
+++ Blog/src/pages/Signup.jsx
@@ -1,8 +1,140 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Label, TextInput, Button, Spinner, Alert } from 'flowbite-react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  //{ ...formData }: This syntax spreads the existing formData object.
+  //[e.target.id]: e.target.value: This is the new key-value pair that we want to add or update in the formData object. It uses bracket notation to dynamically set the key based on the id property (e.target.id), and the value is taken from (e.target.value). This allows us to update the state of a specific form field identified by its id with the value entered by the user.
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value});
+  };
+
+  //use async whenever dealing with databases.
+  //here we are submitting the user data to the database.
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        //post: This indicates that the client is sending data to the server.
+        method: 'POST',
+
+        //it specifies that the content being sent in the request body is JSON data.
+        headers: { 'Content-Type': 'application/json' },
+
+
+        body: JSON.stringify(formData),
+      });
+      
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+  
+
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='p-2 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Jef's
+            </span>
+            Blog
+          </Link>
+
+          <p className='text-sm mt-5'>
+            You can sign up with your email and password or with Google.
+          </p>
+
+        </div>
+        {/* right */}
+
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+
+            <div>
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <div>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </div>
+              ) : (
+                'Sign Up'
+              )}
+            </Button>
+
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
