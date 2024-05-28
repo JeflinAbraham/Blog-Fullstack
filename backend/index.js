107a980d6f7cffe41ee939a2707629d7c389b681
JeflinAbraham <jeflinabraham21@gmail.com>
2024-05-28 16:00:56 +0530

    connected to mongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+console.log(process.env.MONGO);
+mongoose.connect(process.env.MONGO)
+.then(() => {
+  console.log("mongodb is connected")
+})
+.catch((err) => {
+  console.log(err);
+})
 const app = express();
 
 app.listen(3000, () => {
