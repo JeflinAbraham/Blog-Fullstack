89dd4f28af715ecd9277fd353e4daaa4ddecd4e1
JeflinAbraham <jeflinabraham21@gmail.com>
2024-05-29 13:25:56 +0530

    added redux persist feature

--- Blog/src/redux/store.js
+++ Blog/src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.js'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+//why redux-persist? when u refresh the page, the values of redux store veriables should still persist.
+//combineReducers: Combines multiple reducers into a single root reducer.
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+//refer docs.
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+
+export const persistor = persistStore(store);
