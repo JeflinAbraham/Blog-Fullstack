a41289e714bb62aa8255d8d8c7d64f634a0c233a
JeflinAbraham <jeflinabraham21@gmail.com>
2024-06-04 02:44:55 +0530

    created home page

--- Blog/src/pages/Home.jsx
+++ Blog/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
-function Home() {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-col gap-6 justify-center items-center p-80">
+      <h1 className="text-5xl font-bold italic mt-[-20px] animate-bounce">Welcome to my blog</h1>
+      <p className="text-gray-400 text-sm text-center ">
+        Welcome to my corner of the internet! This blog is where I share my thoughts, experiences, and musings on various topics that interest me. From technology to travel, from food to philosophy, you'll find a diverse range of content here. Whether you're a seasoned reader or stumbling upon this page for the first time, I hope you find something that resonates with you.
+      </p>
+    </div>
+  );
 }
-
-export default Home
